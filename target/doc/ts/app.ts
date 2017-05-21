5c460666ef3dd090d7c638577c49a08722d9c6ed
FreeMasen <RFM@Herman.local>
2017-05-20 23:31:45 -0500

    add ElementAttribute to test project

--- target/doc/ts/app.ts
+++ target/doc/ts/app.ts
@@ -1,5 +1,5 @@
 import { HTMLFactory } from './HTMLFactory';
-
+import { ElementAttribute } from './ElementAttribute';
 
 var app;
 window.addEventListener('DOMContentLoaded', function() {
@@ -8,8 +8,9 @@ window.addEventListener('DOMContentLoaded', function() {
 
 class App {
     private factory = new HTMLFactory();
+
     constructor(){
-        var start = this.factory.createElement('h1', 'first-div');
+        var start = this.factory.createElement('h1', new ElementAttribute('id', 'first-div'));
         start.appendChild(document.createTextNode('Hello World'));
         document.body.appendChild(start);
 
