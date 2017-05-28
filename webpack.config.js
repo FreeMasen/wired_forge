7f6faf6669dc0e27b9a9b1dc80d62b20071467f0
FreeMasen <RFM@Herman.local>
2017-05-27 19:18:41 -0500

    update overall style

--- webpack.config.js
+++ webpack.config.js
@@ -18,22 +18,6 @@ module.exports = {
                     test: /\.ts$/,
                     exclude: ['node_modules'],
                     use: ['awesome-typescript-loader']
-                },
-                {
-                    test: /\.scss$/,
-                    use: [{
-                        loader: "style-loader", options: {
-                            sourceMap: true
-                        } // creates style nodes from JS strings
-                    }, {
-                        loader: "css-loader", options: {
-                            sourceMap: true
-                        } // translates CSS into CommonJS
-                    }, {
-                        loader: "sass-loader", options: {
-                            sourceMap: true
-                        } // compiles Sass to CSS
-                    }]
                 }
             ]
         }
