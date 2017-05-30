0695a32f9c2aa8c33b7f115159433126c448f622
FreeMasen <RFM@Herman.local>
2017-05-29 23:29:18 -0500

    add about and login link (no component)

--- target/doc/ts/app.ts
+++ target/doc/ts/app.ts
@@ -1,7 +1,8 @@
-import { HTMLFactory, ElementAttribute } from './HTMLFactory';
+import { HTML, Attribute } from './HTML';
 import { EventHandler } from './EventHandler';
 import { DataService } from './DataService';
 import { BlogPost } from './BlogPost';
+import { About } from './About';
 
 var app;
 window.addEventListener('DOMContentLoaded', function() {
@@ -10,23 +11,82 @@ window.addEventListener('DOMContentLoaded', function() {
 });
 
 class App {
-    private factory = new HTMLFactory();
+    private html = new HTML();
     private events = new EventHandler();
     private data = new DataService();
 
+    private loggedIn = false;
+
     constructor() {
+        this.events.registerHTMLEvent('#login', 'click', this.login, this);
+        this.events.registerHTMLEvent('#home-nav-link','click', this.getPosts, this);
+        this.events.registerHTMLEvent('#about-nav-link', 'click', this.getAbout, this);
+        this.getPosts(null);
+    }
+
+    login(): void {
+        this.loggedIn = !this.loggedIn;
+        var text = this.loggedIn ? 'Logout' : 'Login';
+        var a = this.html.a(text, null, new Attribute('id', 'login'));
+        this.html.addClass(a, 'clickable');
+        // this.events.registerHTMLEvent('#login', 'click', this.login, this);
+        this.html.swapNode('#login', a);
+        this.events.reRegister(a);
+    }
+
+    getPosts(event): void {
+        this.clearMain();
         this.data.get('/rest/blog', (err, posts: Post[]) => {
             if (err) console.error(err);
-            else this.displayPosts(posts);
+            else {
+                if (typeof posts === 'string') {
+                    return console.log(posts);
+                }
+                this.displayPosts(posts);
+            }
         });
     }
 
     displayPosts(posts: Post[]): void {
         let mainContent = document.getElementById('main-content');
         if (mainContent == null) return;
+        var elements = [];
         for (var i = 0; i < posts.length; i++) {
             let post = posts[i];
-            mainContent.appendChild(new BlogPost(post.title, post.content, post.firstname + ' ' + post.lastname).node);
+            elements.push(new BlogPost(post.title, post.content, post.author).node);
+        }
+        this.fillMain(...elements);
+    }
+
+    getAbout(event): void {
+        this.data.get('/rest/about', (err, about: Description[]) => {
+            if (err) return console.error(err);
+            this.displayAbout(about);
+        })
+    }
+
+    displayAbout(sections: Description[]): void {
+        var elements = [];
+        for (var i = 0; i < sections.length; i++) {
+            var section = sections[i];
+            var sectionElement = new About(section.title, section.content);
+            elements.push(sectionElement.node);
+        }
+        this.fillMain(...elements);
+    }
+
+    fillMain(...content: HTMLElement[]): void {
+        this.clearMain()
+        var main = document.getElementById('main-content');
+        if (main === null) return;
+        this.html.addContent(main, content);
+    }
+
+    clearMain(): void {
+        var main = document.getElementById('main-content');
+        if (!main) return;
+        while (main.hasChildNodes()) {
+            main.removeChild(main.lastChild);
         }
     }
 }
@@ -34,6 +94,10 @@ class App {
 class Post {
     title: string
     content: string
-    firstname: string
-    lastname: string
+    author: string
+}
+
+class Description {
+    title: string
+    content: string
 }
