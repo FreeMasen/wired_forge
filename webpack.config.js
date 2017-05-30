88837923267707277d02b02ddec37746b6fc2fe7
FreeMasen <RFM@Herman.local>
2017-05-29 23:26:09 -0500

    moar loaders!

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack');
-
+const ugly = require('uglifyjs-webpack-plugin')
 module.exports = {
         devtool: 'source-map',
         entry: {
@@ -18,7 +18,17 @@ module.exports = {
                     test: /\.ts$/,
                     exclude: ['node_modules'],
                     use: ['awesome-typescript-loader']
+                },
+                {
+                    test: /\.js$/,
+                    use: 'uglify-loader'
                 }
             ]
-        }
+        },
+        plugins: [
+            // new ugly({
+            //     sourceMap: true,
+            //     extractComments: true
+            // })
+        ]
 }
